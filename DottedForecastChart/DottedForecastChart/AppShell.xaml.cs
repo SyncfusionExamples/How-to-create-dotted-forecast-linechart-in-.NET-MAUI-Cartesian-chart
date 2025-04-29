3d15112e6d38cff83178b2aec76034e91fb3cfd6
Harsha <harshamidadhala.k@syncfusion.com>
2025-04-29 10:57:11 +0530

    Version updated

--- DottedForecastChart/DottedForecastChart/AppShell.xaml.cs
+++ DottedForecastChart/DottedForecastChart/AppShell.xaml.cs
@@ -1,9 +1,10 @@
-﻿namespace DottedForecastChart;
-
-public partial class AppShell : Shell
+﻿namespace DottedForecastChart
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
 }
--- DottedForecastChart/DottedForecastChart/Platforms/iOS/AppDelegate.cs
+++ DottedForecastChart/DottedForecastChart/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using Foundation;
 
-namespace DottedForecastChart;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
+namespace DottedForecastChart
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
